514685fe67774060b75c839a372f7b9d760053e0
Crispin R <crispinr003@gmail.com>
2021-09-25 16:27:44 +0530

    Initialize prefix and basic setup

--- app.py
+++ app.py
@@ -2,4 +2,11 @@ import os
 import discord
 import youtube_dl
 from dotenv import load_dotenv
-from discord.ext import command, tasks
+from discord.ext import commands, tasks
+
+load_dotenv()
+discordToken = os.getenv("---discordTokenHere---")
+
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="~", intents=intents)
