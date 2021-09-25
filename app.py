059d5db9cc3f4135817d57b9db5b63c2bbdfaaf9
Crispin R <crispinr003@gmail.com>
2021-09-25 17:23:59 +0530

    Add play command

--- app.py
+++ app.py
@@ -66,3 +66,15 @@ async def leave(ctx):
     else:
         await ctx.sent("Bot is not connected to a voice channel!")
 
+@bot.command(name="play", help="Command bot to play a song")
+async def play(ctx, url):
+    try:
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        await ctx.send("**Now playing: ** {}".format(filename))
+    except:
+        await ctx.send("Bot connected to voice channel!")
