a56cbae23e4afa9e3a9bd24d12d1b63a4fa50a55
Crispin R <crispinr003@gmail.com>
2021-09-27 17:53:01 +0530

    Remove quotes for token var

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ class music_cog(commands.Cog):
             await self.play_music()
 
 bot.add_cog(music_cog(bot))
-bot.run(os.environ.get("DISCORD_TOKEN"))
+bot.run(os.environ.get(DISCORD_TOKEN))
