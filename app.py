447b90657d9ebccdb60457faf5fdd1a3748edd56
Crispin R <crispinr003@gmail.com>
2021-09-27 13:26:36 +0530

    Remove comments

--- app.py
+++ app.py
@@ -25,8 +25,7 @@ Music commands:
 ```
 """
         self.text_channel_list = []
-
-    #some debug info so that we know the bot has started    
+ 
     @commands.Cog.listener()
     async def on_ready(self):
         for guild in self.bot.guilds:
@@ -45,7 +44,6 @@ Music commands:
 
     @commands.command(name="clear", help="Clears a specified amount of messages")
     async def clear(self, ctx, arg):
-        #extract the amount to clear
         amount = 5
         try:
             amount = int(arg)
@@ -56,18 +54,12 @@ bot.add_cog(main_cog(bot))
 class music_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-        #all the music related stuff
         self.is_playing = False
-
-        # 2d array containing [song, channel]
         self.music_queue = []
         self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
         self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-
         self.vc = ""
 
-     #searching the item on youtube
     def search_yt(self, item):
         with YoutubeDL(self.YDL_OPTIONS) as ydl:
             try: 
@@ -81,32 +73,25 @@ class music_cog(commands.Cog):
         if len(self.music_queue) > 0:
             self.is_playing = True
 
-            #get the first url
             m_url = self.music_queue[0][0]['source']
 
-            #remove the first element as you are currently playing it
             self.music_queue.pop(0)
 
             self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
         else:
             self.is_playing = False
 
-    # infinite loop checking 
     async def play_music(self):
         if len(self.music_queue) > 0:
             self.is_playing = True
 
             m_url = self.music_queue[0][0]['source']
-            
-            #try to connect to voice channel if you are not already connected
-
             if self.vc == "" or not self.vc.is_connected() or self.vc == None:
                 self.vc = await self.music_queue[0][1].connect()
             else:
                 await self.vc.move_to(self.music_queue[0][1])
             
             print(self.music_queue)
-            #remove the first element as you are currently playing it
             self.music_queue.pop(0)
 
             self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
@@ -119,7 +104,6 @@ class music_cog(commands.Cog):
         
         voice_channel = ctx.author.voice.channel
         if voice_channel is None:
-            #you need to be connected so that the bot knows where to go
             await ctx.send("Connect to a voice channel!")
         else:
             song = self.search_yt(query)
@@ -148,7 +132,6 @@ class music_cog(commands.Cog):
     async def skip(self, ctx):
         if self.vc != "" and self.vc:
             self.vc.stop()
-            #try to play next in the queue if it exists
             await self.play_music()
 
 bot.add_cog(music_cog(bot))
